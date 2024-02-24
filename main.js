14449cc33b34d6a85b5dc5aa5f619e4f347c4607
Ben.M <benmungovan1@hotmail.com>
2024-02-24 00:22:51 +0000

    added random number generator for later use, and quotes as the outcomes, with an empty list for use later

---
+++ main.js
@@ -0,0 +1,31 @@
+const generateRandomNumber = (num) =>{
+        return Math.floor(Math.random() * num)  
+};
+
+const collectiveOutputs = {
+    motivationalQuotes: [
+      "Believe you can and you're halfway there.",
+      "In the middle of difficulty lies opportunity.",
+      "The only way to do great work is to love what you do.",
+      "You are never too old to set another goal or to dream a new dream.",
+      "You are braver than you believe, stronger than you seem, and smarter than you think."
+    ],
+    inspirationalQuotes: [
+      "The future belongs to those who believe in the beauty of their dreams.",
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+      "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.",
+      "The mind is everything. What you think you become.",
+      "Happiness is not by chance, but by choice."
+    ],
+    wisdomQuotes: [
+      "The only true wisdom is in knowing you know nothing.",
+      "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.",
+      "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+      "Life is a journey, not a destination."
+    ]
+  };
+
+  let personalMessage = []
+
+  
